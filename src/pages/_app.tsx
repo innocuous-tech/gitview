b0c47c6dcf869d39e990f6f7a03eced52b4f8a11
Kyle Holmberg <kylemh.email@gmail.com>
2022-02-03 01:30:04 +0700

    define layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import type { ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import { globalCSSReset, globalStyles } from '~/stitches.config';
+import { Box } from '~/components/Box';
+import Head from 'next/head';
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box
+        as="main"
+        css={{
+          backgroundColor: '$plum1',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          mx: 'auto',
+          minH: '100vh',
+          w: '100%',
+          gap: '$32',
+        }}
+      >
+        {children}
+      </Box>
+    </>
+  );
 }
 
 /**
